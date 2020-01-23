f6fbdc49e50674e914fcbb369e43ad23290a4ca3
Steve Kindt <stevemkindt@gmail.com>
2020-01-22 21:55:38 -0600

    html and css

---
+++ script.js
@@ -0,0 +1,1 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/en-gb.js" integrity="sha256-VWXSrU6D6hQJ7MEZ9062PvZDwmeRuZ8eE/ToQ2N3QjA=" crossorigin="anonymous"></script>
