59bc61968178889af912bfa446e2750aff14fae9
Steve Kindt <stevemkindt@gmail.com>
2020-01-23 16:48:08 -0600

    adding momentjs script

--- script.js
+++
@@ -1,1 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/en-gb.js" integrity="sha256-VWXSrU6D6hQJ7MEZ9062PvZDwmeRuZ8eE/ToQ2N3QjA=" crossorigin="anonymous"></script>
